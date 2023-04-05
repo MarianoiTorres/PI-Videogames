591de7b501095f792287989a18bb45c3853628f4
MarianoiTorres <marianxtorres@gmail.com>
2023-04-05 18:21:13 -0300

    deploy

--- api/index.js
+++ api/index.js
@@ -19,11 +19,11 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-const { DB_PORT } = process.env;
+const { PORT } = process.env;
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(DB_PORT, () => {
-    console.log('%s listening at DB_PORT'); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log('%s listening at ', PORT); // eslint-disable-line no-console
   });
 });
